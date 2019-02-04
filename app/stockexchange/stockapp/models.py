2532a72d0845b03c7a21a5c57373d2adcd20e5ef
Dywinner <33638892+Dywinner@users.noreply.github.com>
2019-02-04 16:53:37 -0500

    Added initial models and migrations

--- app/stockexchange/stockapp/models.py
+++ app/stockexchange/stockapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	username = models.CharField(max_length=30)
+	password = models.CharField(max_length=30)
+	balance = models.DecimalField(max_digits= 30, decimal_places = 2)
+
+class Stock(models.Model):
+	ticker_sym = models.CharField(max_length=5)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
--- app/stockexchange/stockexchange/settings.py
+++ app/stockexchange/stockexchange/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stockapp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'stockexchange.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cs4501',
+        'USER': 'www',
+        'PASSWORD': '$3cureUS',
+        'HOST': 'db',
+
     }
 }
 
