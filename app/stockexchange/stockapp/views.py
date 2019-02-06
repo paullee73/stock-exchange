7a542032dc0b2c49d6f720fd5332a4f06564abbb
paullee73 <paulleechat3@gmail.com>
2019-02-06 13:00:49 -0500

    curl processing

--- app/stockexchange/stockapp/views.py
+++ app/stockexchange/stockapp/views.py
@@ -1,22 +1,50 @@
 from django.shortcuts import render
-from stockapp.models import User
-from stockapp.models import Stock
-from stockapp.forms import CreateForm
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.core import serializers
+from .models import User
+from django.urls import reverse
 
-# Create your views here
-def create_user(request):
-	username='invalid form'
-	password=''
-	balance=''
-	if request.method == "POST":
-		MyForm = CreateForm(request.POST)
-		if MyForm.is_valid():
-			username = MyForm.cleaned_data['username']
-			password = MyForm.cleaned_data['password']
-			balance = MyForm.cleaned_data['balance']
-			m_user = User(username = username, password = password, balance = balance)
-			m_user.save()
-		else:
-			MyForm = CreateForm()
-	return render(request, 'JSONresponse.html', {'username': username, 'password': password, 'balance': balance})
+def index(request):
+    return HttpResponse("Test")
+
+
+def createUser(request):
+    if(request.method == "POST"):
+        username = request.POST['username']
+        password = request.POST['password']
+        balance = float(request.POST['balance'])
+        newUser = User(username = username, password=password, balance=balance)
+        newUser.save()
+
+        return JsonResponse({'username': username, 'password': password, 'balance': balance})
+
+
+def ViewOrUpdateUser(request, uniqueID):
+    if(request.method == "GET"):
+        return JsonResponse(serializers.serialize("json", [User.objects.get(pk=uniqueID),]), safe=False )
+    if(request.method == "POST"):
+        userToUpdate = User.objects.get(pk=uniqueID)
+        username = request.POST['username']
+        password = request.POST['password']
+        balance = request.POST['balance']
+
+        if(username):
+            userToUpdate.username = username
+        if(password):
+            userToUpdate.password = password
+        if(balance):
+        	userToUpdate.balance = balance
+        
+        userToUpdate.save()
+        
+        return JsonResponse(serializers.serialize("json", [userToUpdate]), safe = False)
+
+
+def DeleteUser(request, uniqueID):
+    if(request.method == "POST"):
+    	userToDelete = User.objects.get(pk=uniqueID)
+    	username = userToDelete.username
+    	password = userToDelete.password
+    	balance = userToDelete.balance
+    	userToDelete.delete()
+    	return JsonResponse({'username': username, 'password': password, 'balance': balance})
--- app/stockexchange/stockexchange/settings.py
+++ app/stockexchange/stockexchange/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #s'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
