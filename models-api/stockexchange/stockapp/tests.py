e7104b6e970d1067497b9b297bae29c3008552a1
paullee73 <paulleechat3@gmail.com>
2019-02-20 21:18:04 -0500

    tests

--- models-api/stockexchange/stockapp/tests.py
+++ models-api/stockexchange/stockapp/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from stockapp.models import Stock, User
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your tests here.
+
+class GetOrderDetailsTestCase(TestCase):
+    # setUp method is called before each test in this class
+    def setUp(self):
+        newUser = User(username='Joe', password='bread', balance=float(1500))
+        newUser.save()
+        pass  # nothing to set up
+
+    def test_creation(self):
+        try:
+            user = User.objects.get(username='Joe')
+            self.assertEquals(user.password, 'bread')
+        except ObjectDoesNotExist:
+            self.fail('Error')
+
+    def test_read(self):
+        user = User.objects.get(username='Joe')
+        self.assertEquals(user.password, 'bread')
+
+    def test_update(self):
+        user = User.objects.get(username='Joe')
+        user.password = 'dog'
+        self.assertEquals(user.password, 'dog')
+
+    def test_delete(self):
+        user = User.objects.get(username='Joe')
+        user.delete()
+        try:
+            retrieval = User.objects.get(username='Joe')
+            pass
+        except ObjectDoesNotExist:
+            self.fail('Error')
+
+    # tearDown method is called after each test
+
+    def tearDown(self):
+        pass  # nothing to tear down
