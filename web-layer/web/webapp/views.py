a423cc56e0fe2e3b1de0229073847e5f42b2da38
paullee73 <paulleechat3@gmail.com>
2019-03-07 16:52:34 -0500

    base header file

--- web-layer/web/webapp/views.py
+++ web-layer/web/webapp/views.py
@@ -8,21 +8,32 @@ import urllib.error
 import json
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse("Test")
 
+
+def displayHome(request):
+    if(request.method == 'GET'):
+        return render(request, 'base_header.html')
+
+
 def displayStocks(request):
-	if (request.method == 'GET'):
-		req = urllib.request.Request("http://exp-api:8000/exp/stock/item_detail")
-		resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-		resp = json.loads(resp_json)
-		resplist = resp['data']
-	return render(request, 'item_detail.html', {'resplist' : resplist})
+    if (request.method == 'GET'):
+        req = urllib.request.Request(
+            "http://exp-api:8000/exp/stock/item_detail")
+        resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+        resp = json.loads(resp_json)
+        resplist = resp['data']
+    return render(request, 'item_detail.html', {'resplist': resplist})
+
 
 def userDetail(request, uniqueID):
-	if (request.method == 'GET'):
-		req = urllib.request.Request("http://exp-api:8000/exp/user/" + uniqueID + "")
-		resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-		resp = json.loads(resp_json)
-	#return JsonResponse({'resp' : resp})
-	return render(request, 'user_detail.html', {'resp' : resp})
+    if (request.method == 'GET'):
+        req = urllib.request.Request(
+            "http://exp-api:8000/exp/user/" + uniqueID + "")
+        resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+        resp = json.loads(resp_json)
+    # return JsonResponse({'resp' : resp})
+    return render(request, 'user_detail.html', {'resp': resp})
